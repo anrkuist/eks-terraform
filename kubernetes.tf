83c0ceec8d0543c3a3d9e794ac2445a7aabcefdf
anrkuist <anrkuist@protonmail.com>
2022-02-18 22:11:24 -0300

    Changed module attribute

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,9 +1,9 @@
 data "aws_eks_cluster" "default" {
-  name = var.cluster_name
+  name = module.eks.cluster_id 
 }
 
 data "aws_eks_cluster_auth" "default" {
-  name = var.cluster_name
+  name = module.eks.cluster_id 
 }
 
 provider "kubernetes" {
@@ -14,9 +14,9 @@ provider "kubernetes" {
 
 resource "local_file" "kubeconfig" {
   sensitive_content = templatefile("${path.module}/kubeconfig.tpl", {
-    cluster_name = var.cluster_name,
+    cluster_name = module.eks.cluster_id,
     clusterca    = data.aws_eks_cluster.default.certificate_authority[0].data,
     endpoint     = data.aws_eks_cluster.default.endpoint,
     })
-  filename          = "./kubeconfig-${var.cluster_name}"
+  filename          = "./kubeconfig-${module.eks.cluster_id}"
 }
